770b43d3542ca32d3907aa02d19314b9284a2bc9
Soham Annadate <sohamannadate007@gmail.com>
2026-07-17 19:58:45 +0530

    pip & modules done

--- Day 1/module.py
+++ Day 1/module.py
@@ -2,3 +2,7 @@ import pyjokes
 
 joke = pyjokes.get_joke()
 print(joke)
+
+
+# pip : it is a package manager for python. Can get used for installing the different modules (like, flask, django, etc)
+# modules : a file containing code written by somebody else(usually) which can be imported and used in our programs

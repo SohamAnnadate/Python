972cff357e1cc6ff3bf7a6b0eadb721fad2b377c
Soham Annadate <sohamannadate007@gmail.com>
2026-07-17 20:09:47 +0530

    basics done

--- Day 1/Hello.py
+++ Day 1/Hello.py
@@ -1,9 +1,27 @@
 print("Hello")
 
 
+# REPL : Read Evaluate Print Loop 
+# we can use python as an calculator 
+# STEPS : 
+#  1. it reads the data (5+5)
+#  2. it evaluated it (solving the problem)
+#  3. prints the result (10)
+#  4. goes for next instruction
+
+# python runs a single line based on cmd (command line interface)
+# one line at a time 
+
+
+
 # cntl + / : add the single line comment 
 
 # This is a simple python program 
-    
+    # it is a single line comment
+
+""" 
+  ..................
+"""
+    # it is a multiline comment 
     
     

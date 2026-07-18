598ccafe42b061426ad81eff93ce8b2efe9390a9
Soham Annadate <sohamannadate007@gmail.com>
2026-07-18 12:27:58 +0530

    Variables Rules done

---
+++ Day 2/03_rule_Variables.py
@@ -0,0 +1,13 @@
+# A variable name can contain "ALPHABETs", "DIGITs", and "UNDERSCOREs".
+# A variable name can only START with an "ALPHABETs" and "UNDERSCOREs".
+# A variable name CAN'T START with a "DIGIT".
+# "NO WHITE SPACE" is allowed to be used inside a variable name.
+
+# No special characters are allowed to decalare the variable name [except '_' (allowed)]
+
+name = "Soham" # Valid
+_name = "Rahul" # Valid
+name_8 = "Sam" # Valid
+
+# 8name = "Adi" # Invalid
+# first name = "adi" #Invalid

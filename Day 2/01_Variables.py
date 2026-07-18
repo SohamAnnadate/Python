7c571568a8e026e68355f12a0250167e8e9ded44
Soham Annadate <sohamannadate007@gmail.com>
2026-07-18 12:14:09 +0530

    Variables done

---
+++ Day 2/01_Variables.py
@@ -0,0 +1,16 @@
+# Variable is a name given to the memory location which stores the data 
+# Variable is also called as IDENTIFIER which identifies the memory location/data 
+# Variables is a container which stores the value/ any data into the variable
+# Data should by any types like (integer, float, stirings, booleans, none, etc)
+
+a = 10 
+print(a)
+print(type(a))
+
+b = 5.65 
+print(b)
+print(type(b))
+
+name = "Soham"
+print(name)
+print(type(name))

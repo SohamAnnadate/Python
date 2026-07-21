f43b3787dd41ea052c3da06017963c6b249acfd0
Soham Annadate <sohamannadate007@gmail.com>
2026-07-21 19:56:33 +0530

    Working of input() function is done

---
+++ Day 2/06_input.py
@@ -0,0 +1,13 @@
+# input() function is used for taking the input form the user.
+
+a = input("Enter the a :")
+print(a)
+print(type(a))
+
+# it accepts the values in the form of "String"
+# to accept the values in the correct datatype we need to rewrite the input() function with the datatype function like : int(), float(), etc
+
+x = int(input("Enter the A : "))
+y = int(input("Enter the B : "))
+
+z = print("Sun is : ", x + y)

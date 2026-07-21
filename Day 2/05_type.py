561e9acd6c657cb6d9be1aef84756bd95514ae1e
Soham Annadate <sohamannadate007@gmail.com>
2026-07-21 19:50:01 +0530

    type() finction and type casting is done

---
+++ Day 2/05_type.py
@@ -0,0 +1,22 @@
+# type() Function :
+# type() function is used to find the data types of a given variables in python.
+
+# a = 31
+# print(type(a)) # class <int>
+
+# b = '31'
+# print(type(b)) # class <str>
+
+# Changing the data types form one form to another form is done by type casting...
+
+# Suppose : 
+ 
+a = "31.3"
+print(type(a)) # class <str>
+
+b = float(a) # float() function changes the type of a into the float datatype 
+print(type(b))
+
+# it like that we can use other functions lile :
+# float(), ini(), string(), double(), etc
+# to do the typecasting.

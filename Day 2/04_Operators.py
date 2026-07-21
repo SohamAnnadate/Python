e769b68d38d64873b8a1d933d63115d70dd62156
Soham Annadate <sohamannadate007@gmail.com>
2026-07-21 19:29:37 +0530

    Arithmetic, Comparison, Assignemnt, Logical Operators are Done

--- Day 2/04_Operators.py
+++ Day 2/04_Operators.py
@@ -1,1 +1,99 @@
 #Operators required for python program...!
+
+#Arithmetic Functions :
+x = 10
+y = 5
+
+a = 15
+b = 20
+c = 20
+d = 30 
+e = 35
+
+# Addition :
+a = x + y
+print(a)
+
+# Subtraction :
+b = x - y
+print(b)
+
+# Multiplication :
+c = x * y
+print(c)
+
+# Division :
+d = x / y
+print(d)
+
+# Modulo :
+e = x % y
+print(e)
+
+
+# Assignment Operator : 
+
+# ' = '
+a = a + 5
+print(a)
+
+# ' += ' 
+b += 5
+print(b)
+
+# ' -= '
+c -= 2
+print(c)
+
+# ' *= '
+d *= 2
+print(d)
+
+# ' /= '
+e /= 5
+print(e)
+
+# Comparison Operator :
+
+x = a == b
+print(x)
+
+x = e > b
+print(x)
+
+x = e < b
+print(x)
+
+x = e >= b
+print(x)
+
+x = e <= b
+print(x)
+
+x = e != b
+print(x)
+
+# Logical Operators :
+
+z = True or False
+print(z)
+
+# Truth Table of 'OR'
+
+print("True OR False is :", True or False)
+print("True OR True is :", True or True)
+print("False OR True is :", False or True)
+print("False OR False is :", False or False) 
+print("")
+
+#  Truth Table of 'AND'
+
+print("True and False is :", True and False)
+print("True and True is :", True and True)
+print("False and True is :", False and True)
+print("False and False is :", False and False) 
+
+# Not
+
+e = not(True)
+print(e)

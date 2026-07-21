4dc1b13ac510a262b5bff1fa07174b148e766e67
Soham Annadate <sohamannadate007@gmail.com>
2026-07-21 22:14:09 +0530

    Problem 5 is done

--- Day 2 PS/Problem5.py
+++ Day 2 PS/Problem5.py
@@ -1,14 +1,7 @@
 # 5. Write a python program to find an average of two numbers entered by the user. 
 
-# a = int(input("Enter Value of A :"))
-# b = int(input("Enter Value of B :"))
-
-# avergae = (a + b) / 2
-# print(avergae)
-
-import math
-
 a = int(input("Enter Value of A :"))
 b = int(input("Enter Value of B :"))
 
-print(AVG(a,b))
+avergae = (a + b) / 2
+print(avergae)

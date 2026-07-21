4e07fd60f90647177da6568d18b2a1d9a51c3365
Soham Annadate <sohamannadate007@gmail.com>
2026-07-21 20:06:30 +0530

    Problem 2 is done

---
+++ Day 2 PS/Problem2.py
@@ -0,0 +1,18 @@
+# 2. Write a python program to find remainder when a number is divided by z.
+
+# Direct Way using the values :
+x = 25
+z = 4
+result = x % z
+print(result)
+
+# Using the input() function :
+
+divident = int(input("Enter the Divident : "))
+divisior = int(input("Enter the Divisior : "))
+
+quotient = divident / divisior
+print("Quotient is : ", quotient)
+
+remainder = divident % divisior
+print("Remainder is : ", remainder)

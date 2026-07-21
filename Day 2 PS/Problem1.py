aa1f5a8411cbc8594de4e34810d300d87bbf954b
Soham Annadate <sohamannadate007@gmail.com>
2026-07-21 20:01:07 +0530

    Problem 1 is Done

---
+++ Day 2 PS/Problem1.py
@@ -0,0 +1,14 @@
+# 1. Write a python program to add two numbers.
+
+# Direct Way : 
+
+a = 5
+b = 5
+print(a + b)
+
+# By using input() function : 
+
+a = int(input("Enter value of A : "))
+b = int(input("Enter value of B : "))
+
+print("Sum is : ", a + b)

98ad90632577165dccf4f3c5374a31a35021cb28
Soham Annadate <sohamannadate007@gmail.com>
2026-07-22 22:03:08 +0530

    Slicing intro done...

---
+++ Day 3/01_Intro_to_Strings.py
@@ -0,0 +1,13 @@
+# String is a datatype in a python.
+# String is a sequence of characters enclosed in quotes.
+# We can primarily write a string in three ways.
+
+a = "Soham"
+b = 'Sam'
+c = '''My name is soham'''
+
+print(a)
+print(b)
+print(c) 
+
+# String Slicing.....

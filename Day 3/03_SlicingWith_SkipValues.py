a2504e4ffb28286b37267799a8d9ea781da02f2d
Soham Annadate <sohamannadate007@gmail.com>
2026-07-26 15:47:36 +0530

    String Slicing is Done

---
+++ Day 3/03_SlicingWith_SkipValues.py
@@ -0,0 +1,15 @@
+# Example : 
+'''
+    name = "IndiaIsMyCountry"
+    str = name[0 : 7 : 2]
+    print(str)
+    
+    0 to 7 = "IndiaIs"
+    2 Spacing Skips : Idas
+    
+    It prints the part of string from 0th index to 6th index and only takes the 2nd value
+'''
+
+name = "IndiaIsMyCountry"
+str = name[0 : 7 : 2]
+print(str)

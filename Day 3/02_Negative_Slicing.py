3c3de02f84f2ae873b266e6272a63ebf9d8c23f5
Soham Annadate <sohamannadate007@gmail.com>
2026-07-26 15:41:19 +0530

    Negative Slicing is Done

---
+++ Day 3/02_Negative_Slicing.py
@@ -0,0 +1,7 @@
+# [ S  O  H  A  M ]  #String Characters
+# [ 0  1  2  3  4 ]  #from 0 to len-1
+# [-5 -4 -3 -2 -1]   #from negative numbers form last to first
+
+name = "SohamAnnadate"
+name2 = name[-5 : -1] # Negative Slicing....
+print(name2)

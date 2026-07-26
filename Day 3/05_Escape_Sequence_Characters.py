c1d686d554cb88f6c667f3c2104ea3247240b403
Soham Annadate <sohamannadate007@gmail.com>
2026-07-26 16:07:08 +0530

    Escape Sequence Characters in Python

---
+++ Day 3/05_Escape_Sequence_Characters.py
@@ -0,0 +1,46 @@
+# Sequence of characters after backslash "\" are called Escape Sequence characters.
+# These characters represent one special character inside strings
+
+'''
+\n : new line
+\t : tab
+\' : single quote
+\\ : backslash
+'''
+# Escape Sequence Characters in Python
+
+# 1. \n (New Line)
+print("Hello\nWorld")
+# Output:
+# Hello
+# World
+
+
+# 2. \t (Horizontal Tab)
+print("Name\tAge")
+# Output:
+# Name    Age
+
+
+# 3. \\ (Backslash)
+print("C:\\Users\\Soham")
+# Output:
+# C:\Users\Soham
+
+
+# 4. \' (Single Quote)
+print('It\'s Python')
+# Output:
+# It's Python
+
+
+# 5. \" (Double Quote)
+print("He said, \"Hello!\"")
+# Output:
+# He said, "Hello!"
+
+
+# 6. \b (Backspace)
+print("Helloo\b")
+# Output:
+# Hello

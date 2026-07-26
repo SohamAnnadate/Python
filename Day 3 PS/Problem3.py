4c69908694ccfd46867de784798b52f049c420fd
Soham Annadate <sohamannadate007@gmail.com>
2026-07-26 16:37:59 +0530

    Day 5 Problem Set

---
+++ Day 3 PS/Problem3.py
@@ -0,0 +1,4 @@
+# 3. Write a program to detect double space in a string
+
+name = "My Name is  Sham"
+print(name.find("  "))
---
+++ Day 3 PS/Problem4.py
@@ -0,0 +1,9 @@
+# 4. Replace the double space from problem 3 with single spaces. 
+
+name = "My Name is  Sham"
+print(name.replace("  ", " "))
+
+
+
+# Strings are immutable which means you cannot change its value once declared...
+# Each functions returns the copy of the old string with its updated value

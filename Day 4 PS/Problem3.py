fd0a28a286c21a9a3054e585aac5d38268006f93
Soham Annadate <sohamannadate007@gmail.com>
2026-07-27 23:33:39 +0530

    Problem Set (List and Tuple)

---
+++ Day 4 PS/Problem3.py
@@ -0,0 +1,4 @@
+# 3. Check that a tuple type cannot be changed in python.
+
+a = ("sam", 52, 5.1)
+a[2] = "sai"
---
+++ Day 4 PS/Problem4.py
@@ -0,0 +1,4 @@
+# 4. Write a program to sum a list with 4 numbers.
+
+marks = [54, 56, 80, 50]
+print(sum(marks))
